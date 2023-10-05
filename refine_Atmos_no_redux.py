1a8ac29ad6e3e73ead6afc9ac36a68f442f1c01d
Chris Blanton <Chris.Blanton@an014.princeton.rdhpcs.noaa.gov>
2023-10-05 17:29:55 -0400

    Remove unneeded tracer and albedo calculations

--- refine_Atmos_no_redux.py
+++ refine_Atmos_no_redux.py
@@ -17,11 +17,7 @@ extra_time_variables = ["time_bnds", "average_T1", "average_T2", "average_DT"]
 do_not_encode_vars = ["nv", "grid_xt", "grid_yt", "time", "lev"]
 grid_vars = ["grid_xt", "grid_yt", "ap", "b", "ap_bnds", "b_bnds", "lev", "lev_bnds"]
 unaccepted_variables_for_masking = ["cll", "clm", "clh"]
-albedo_shortname = "albs"
 surf_pres_short = "ps"
-albedo_metadata = dict(
-    long_name="Surface Albedo", units="1.0", standard_name="surface_albedo"
-)
 pkgname = "xrefineDiags"
 scriptname = "refine_Atmos_no_redux.py"
 pro = f"{pkgname}/{scriptname}"
@@ -73,24 +69,6 @@ def run():
         write_dataset(refined, ds, pressure_vars, args)
 
     return None
-
-
-def compute_albedo(ds, swdown="rsds", swup="rsus"):
-    """compute surface albedo from upwelling and downwelling shortwave radiation rsus/rsds"""
-
-    albedo = xr.where(ds[swdown] > 0.0, ds[swup] / ds[swdown], CMOR_MISSING_VALUE)
-
-    # copy attributes from input field
-    albedo.attrs = ds[swdown].attrs.copy()
-    # remove interp_method for fregrid
-    if "interp_method" in albedo.attrs:
-        albedo.attrs.pop("interp_method")
-    # update name and units
-    albedo.attrs.update(albedo_metadata)
-    # add comment on how albedo was computed
-    albedo.attrs.update({"comment": f"{swup}/{swdown}"})
-
-    return albedo
 
 
 def mask_above_surface_pressure(
@@ -170,157 +148,6 @@ def pressure_coordinate(ds, varname, verbose=False):
             print(f"{pro}: {varname} has no pressure coords")
 
     return pressure_coord
-
-
-def refine_tracers(ds, refined, new_vars_output, verbose=False):
-    """compute additional tracers"""
-
-    all_vars = set(ds.variables)
-
-    if set(["emipoa", "chepsoa"]).issubset(all_vars):
-        new_vars_output = True
-        refined["emioa"] = ds["emipoa"] + ds["chepsoa"]
-        refined["emioa"].attrs = ds["emipoa"].attrs.copy()
-        refined["emioa"].attrs.update(
-            {
-                "long_name": "Rate of Emission and Production of Dry Aerosol Total Organic Matter",
-                "units": "kg m-2 s-1",
-                "comment": "emipoa+chepsoa",
-                "standard_name": "tendency_of_atmosphere_mass_content_of_particulate_organic_matter_dry_aerosol_due_to_net_chemical_production_and_emission",
-            }
-        )
-
-    if set(["emiapoa", "chepasoa"]).issubset(all_vars):
-        new_vars_output = True
-        refined["emiaoa"] = ds["emiapoa"] + ds["chepasoa"]
-        refined["emiaoa"].attrs = ds["emiapoa"].attrs.copy()
-        refined["emiaoa"].attrs.update(
-            {
-                "long_name": "total emission of anthropogenic organic aerosol",
-                "units": "kg m-2 s-1",
-                "comment": "emiapoa+chepasoa",
-                "standard_name": "tendency_of_atmosphere_mass_content_of_particulate_organic_matter_dry_aerosol_particles_due_to_net_chemical_production_and_emission",
-            }
-        )
-
-    if set(["drypoa", "drysoa"]).issubset(all_vars):
-        new_vars_output = True
-        refined["dryoa"] = ds["drypoa"] + ds["drysoa"]
-        refined["dryoa"].attrs = ds["drypoa"].attrs.copy()
-        refined["dryoa"].attrs.update(
-            {
-                "long_name": "Dry Deposition Rate of Dry Aerosol Total Organic Matter",
-                "units": "kg m-2 s-1",
-                "comment": "drypoa+drysoa",
-                "standard_name": "tendency_of_atmosphere_mass_content_of_particulate_organic_matter_dry_aerosol_due_to_dry_deposition",
-            }
-        )
-
-    if set(["wetpoa", "wetsoa"]).issubset(all_vars):
-        new_vars_output = True
-        refined["wetoa"] = ds["wetpoa"] + ds["wetsoa"]
-        refined["wetoa"].attrs = ds["wetpoa"].attrs.copy()
-        refined["wetoa"].attrs.update(
-            {
-                "long_name": "Wet Deposition Rate of Dry Aerosol Total Organic Matter",
-                "units": "kg m-2 s-1",
-                "comment": "wetpoa+wetsoa",
-                "standard_name": "tendency_of_atmosphere_mass_content_of_particulate_organic_matter_dry_aerosol_due_to_wet_deposition",
-            }
-        )
-
-    if set(["pso4_aq_kg_m2_s", "pso4_aq_so2_reevap_ls"]).issubset(all_vars):
-        new_vars_output = True
-        refined["cheaqpso4"] = ds["pso4_aq_kg_m2_s"] + ds["pso4_aq_so2_reevap_ls"]
-        refined["cheaqpso4"].attrs = ds["pso4_aq_kg_m2_s"].attrs.copy()
-        refined["cheaqpso4"].attrs.update(
-            {
-                "long_name": "Aqueous-phase Production Rate of Sulfate Aerosol",
-                "units": "kg m-2 s-1",
-                "comment": "pso4_aq_kg_m2_s+pso4_aq_so2_reevap_ls",
-                "standard_name": "tendency_of_atmosphere_mass_content_of_sulfate_dry_aerosol_particles_due_to_aqueous_phase_net_chemical_production",
-            }
-        )
-
-    if set(["eminox_woL", "eminox_lght"]).issubset(all_vars):
-        new_vars_output = True
-        refined["eminox"] = ds["eminox_woL"] + ds["eminox_lght"]
-        refined["eminox"].attrs = ds["eminox_woL"].attrs.copy()
-        refined["eminox"].attrs.update(
-            {
-                "long_name": "Total Emission Rate of NOx",
-                "units": "kg m-2 s-1",
-                "comment": "eminox_woL+eminox_lght",
-                "standard_name": "tendency_of_atmosphere_mass_content_of_nox_expressed_as_nitrogen_due_to_emission",
-            }
-        )
-
-    if set(["emiisop_woB", "emiisop_biogenic"]).issubset(all_vars):
-        new_vars_output = True
-        refined["emiisop"] = ds["emiisop_woB"] + ds["emiisop_biogenic"]
-        refined["emiisop"].attrs = ds["emiisop_woB"].attrs.copy()
-        refined["emiisop"].attrs.update(
-            {
-                "long_name": "Total Emission Rate of Isoprene",
-                "units": "kg m-2 s-1",
-                "comment": "emiisop_woB+emiisop_biogenic",
-                "standard_name": "tendency_of_atmosphere_mass_content_of_isoprene_due_to_emission",
-            }
-        )
-
-    if set(["eminh3_woOCN", "nh3_mol_flux_atm0"]).issubset(all_vars):
-        new_vars_output = True
-        refined["eminh3"] = ds["eminh3_woOCN"] + 0.017 * ds["nh3_mol_flux_atm0"]
-        refined["eminh3"].attrs = ds["eminh3_woOCN"].attrs.copy()
-        refined["eminh3"].attrs.update(
-            {
-                "long_name": "Total Emission Rate of NH3",
-                "units": "kg m-2 s-1",
-                "comment": "eminh3_woOCN+0.017*nh3_mol_flux_atm0",
-                "standard_name": "tendency_of_atmosphere_mass_content_of_ammonia_due_to_emission",
-            }
-        )
-
-    if set(["drynh3_woOCN", "nh3_mol_flux_atm0", "nh3_mol_flux"]).issubset(all_vars):
-        new_vars_output = True
-        refined["drynh3"] = (
-            ds["drynh3_woOCN"]
-            + 0.017 * ds["nh3_mol_flux_atm0"]
-            - 0.017 * ds["nh3_mol_flux"]
-        )
-        refined["drynh3"].attrs = ds["drynh3_woOCN"].attrs.copy()
-        refined["drynh3"].attrs.update(
-            {
-                "long_name": "Dry Deposition Rate of NH3",
-                "units": "kg m-2 s-1",
-                "comment": "drynh3_woOCN+0.017*nh3_mol_flux_atm0-0.017*nh3_mol_flux",
-                "standard_name": "minus_tendency_of_atmosphere_mass_content_of_ammonia_due_to_dry_deposition",
-            }
-        )
-
-    if set(
-        ["dust1_flux", "dust2_flux", "dust3_flux", "dust4_flux", "dust5_flux"]
-    ).issubset(all_vars):
-        new_vars_output = True
-        refined["emidust"] = (
-            ds["dust1_flux"]
-            + ds["dust2_flux"]
-            + ds["dust3_flux"]
-            + ds["dust4_flux"]
-            + ds["dust5_flux"]
-        )
-        refined["emidust"] = refined["emidust"].clip(vmin=0.0)
-        refined["emidust"].attrs = ds["dust1_flux"].attrs.copy()
-        refined["emidust"].attrs.update(
-            {
-                "long_name": "Total Emission Rate of Dust",
-                "units": "kg m-2 s-1",
-                "comment": "dust1_flux+dust2_flux+dust3_flux+dust4_flux+dust5_flux",
-                "standard_name": "tendency_of_atmosphere_mass_content_of_dust_dry_aerosol_particles_due_to_emission",
-            }
-        )
-
-    return refined, new_vars_output
 
 
 def write_dataset(ds, template, pressure_vars, args):
@@ -403,22 +230,6 @@ def parse_args():
         help="Print detailed output",
     )
     parser.add_argument("-t", "--tagfile", action="store_true", required=False, help="")
-    parser.add_argument(
-        "-d",
-        "--shortwave_down",
-        type=str,
-        required=False,
-        default="rsds",
-        help="name of downwards shortwave radiation",
-    )
-    parser.add_argument(
-        "-u",
-        "--shortwave_up",
-        type=str,
-        required=False,
-        default="rsus",
-        help="name of upwards shortwave radiation",
-    )
     parser.add_argument(
         "-f",
         "--format",
